486871e9a470c039bb9221f4aa5b3654913d07b1
Heer Gosrani <gosraniheer@gmail.com>
2025-05-07 23:25:32 +0530

    half-done

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,6 @@
-import heroCss from "./HeroSection.module.css"
+import heroCss from "./HeroSection.module.css";
+import person from "./../../assets/img/heer.png"
+import { HiDownload } from "react-icons/hi";
 
 function HeroSection(){
     return(
@@ -6,10 +8,27 @@ function HeroSection(){
            <section>
                 <div className={heroCss.container}>
                     <div className={heroCss.intro}>
-                        <p>Hello I'm <span>Heer Gosrani</span>,</p>
+                        <p>Hello, </p>
                     </div>
+
+                    <div className={heroCss.intro1}>
+                        <p> I'm <span>Heer Gosrani</span>,</p>
+                    </div> 
+
                     <div className={heroCss.myRole}>
                         <p>a <span>Software</span> Developer.</p>
+                    </div>
+
+                    <div className={heroCss.heroImg}>
+                        <img src={person} alt="person" />
+                    </div>
+
+                    <div className={heroCss.glass}>
+                        <p>- I'm creative developer, and designer based in India, and I'm very passionate and dedicated to my work.</p>
+                    </div>
+
+                    <div className={heroCss.downloadCv}>
+                        <p> - Download Cv <HiDownload className={heroCss.down}/></p>
                     </div>
                 </div>
            </section>
