054d5c5f705671320e35876cfc58539ac203ebf7
Heer Gosrani <gosraniheer@gmail.com>
2025-05-09 13:53:44 +0530

    aboutSection-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from "./components/Navbar/Navbar.jsx"
 import HeroSection from './components/HeroSection/HeroSection.jsx'
+import AboutSection from './components/AboutSection/AboutSection.jsx'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </>
   )
 }
